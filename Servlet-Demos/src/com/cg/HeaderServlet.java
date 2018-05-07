05b04909a8157b2b07bfd58d8b81b00e9c35199b
abhishek-ghode <abhishek.ghode@capgemini.com>
2018-05-07 12:08:44 +0530

    Added a demo on servlet

---
+++ Servlet-Demos/src/com/cg/HeaderServlet.java
@@ -0,0 +1,50 @@
+package com.cg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HeaderServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(request, response);
+		
+		Enumeration<String> headerNames = request.getHeaderNames();
+		
+		response.setContentType("text/xml");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		
+		out.println("<table border=\"1\">");
+		
+		while(headerNames.hasMoreElements()){
+			
+			out.println("<tr>");
+			
+			String name = headerNames.nextElement();
+			String value = request.getHeader(name);
+			
+			out.println("<td>"+name+"</td>");
+			out.println("<td>"+value+"</td>");
+			
+			out.println("</tr>");
+			
+		}
+		
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+	
+}
